fe1d4942b420d73682fad83077b6900697c3d51f
Soran Ismail <soran.i@outlook.com>
2022-02-09 00:02:15 -0500

    Changed some stuff

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -2,29 +2,30 @@ AOS.init();
 
 //  Work experience cards
 
-
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
     title: "Mechatronics Engineer Intern",
-    cardImage: "assets/images/experience-page/wabtec-corporation-logo-vector.svg",
+    cardImage:
+      "assets/images/experience-page/wabtec-corporation-logo-vector.svg",
     place: "Wabtec Corporation",
     time: "(May 2021 - Present)",
     desp: "<li></li><li></li><li></li>",
   },
   {
     title: "Student Software Engineer",
-    cardImage: "assets/images/experience-page/western-university-vector-logo.svg",
+    cardImage:
+      "assets/images/experience-page/western-university-vector-logo.svg",
     place: "Department of Earth Sciences, Western University",
     time: "(May 2020 - April 2021)",
-    desp: "<li>Migrated app to Google Maps from Mapbox API, increasing battery life by <b>32%</b> </li><li>Identified memory leaks in objective-C using XCode debugging tools and repaired them by eliminating retain cycles</li><li><b>Leveraged knowledge in</b> automatic reference counting, Google Maps, Mapbox, Scrum \& Agile development framework, Model View Controller design pattern</li>",
+    desp: "<li>Migrated app to Google Maps from Mapbox API, increasing battery life by <b>32%</b> </li><li>Identified memory leaks in objective-C using XCode debugging tools and repaired them by eliminating retain cycles</li><li><b>Leveraged knowledge in</b> automatic reference counting, Google Maps, Mapbox, Scrum & Agile development framework, Model View Controller design pattern</li>",
   },
   {
     title: "Electrical Engineer Intern",
     cardImage: "assets/images/experience-page/Magna_logo.svg",
     place: "Magna International",
     time: "(May 2019 - Sep 2019)",
-    desp:"<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    desp: "<li>Designed and constructed schematic for a torque robot control panel, consisting of safety relays, PLC IO modules, and other rack devices for electrical safety authority approval</li><li>Utilized Solidworks with 3D printing to solve <b>18</b> safety and ergonomic issues with a maximum <b>3</b> day lead time as they arose on the assembly line</li> <li>Developed a quality control program using Python to detect failures in robot inspection pictures requiring further verification</li> <li><b>Leveraged knowledge in </b> HTML, CSS, Javascript (ES6), Docker, Selenium, pyTesseract, and FANUC Robots </li>",
   },
 ];
 
@@ -45,9 +46,9 @@ const showCards2 = () => {
               <span class="pre-heading">${place}</span><br>
               <span class="author">${time}</span>
             </p>
-            <ol>
+            <ul>
               ${desp}
-            </ol>
+            </ul>
           </header>
         </article>
       </div>
@@ -58,36 +59,35 @@ const showCards2 = () => {
 };
 document.addEventListener("DOMContentLoaded", showCards2);
 
-
 // Volunteership Cards
 
-const volunteership = document.querySelector(".volunteership");
-const volunteershipcards = [
-  {
-    title: "GirlScript Summer Of Code 2020",
-    cardImage: "assets/images/experience-page/1.jpg",
-    description:
-      "Responsible for handling the projects GirlScript App and GirlScript Website Boilerplate.",
-  },
-  {
-    title: "StudentCode-in 2020",
-    cardImage: "assets/images/experience-page/2.jpg",
-    description:
-      "Responsible for handling open source contributions for the project Awesome Developer Portfolio.",
-  },
-  {
-    title: "PClub Summer Of Code 2020",
-    cardImage: "assets/images/experience-page/3.jpg",
-    description:
-      "Mentoring for the projects Deep Pixel, AutoVaidya, Just Resume and Doc2pen.",
-  },
-  {
-    title: "Hakin-Codes",
-    cardImage: "assets/images/experience-page/4.jpg",
-    description:
-      "Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen.",
-  },
-];
+// const volunteership = document.querySelector(".volunteership");
+// const volunteershipcards = [
+//   {
+//     title: "GirlScript Summer Of Code 2020",
+//     cardImage: "assets/images/experience-page/1.jpg",
+//     description:
+//       "Responsible for handling the projects GirlScript App and GirlScript Website Boilerplate.",
+//   },
+//   {
+//     title: "StudentCode-in 2020",
+//     cardImage: "assets/images/experience-page/2.jpg",
+//     description:
+//       "Responsible for handling open source contributions for the project Awesome Developer Portfolio.",
+//   },
+//   {
+//     title: "PClub Summer Of Code 2020",
+//     cardImage: "assets/images/experience-page/3.jpg",
+//     description:
+//       "Mentoring for the projects Deep Pixel, AutoVaidya, Just Resume and Doc2pen.",
+//   },
+//   {
+//     title: "Hakin-Codes",
+//     cardImage: "assets/images/experience-page/4.jpg",
+//     description:
+//       "Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen.",
+//   },
+// ];
 
 const showCards = () => {
   let output = "";
@@ -108,9 +108,7 @@ const showCards = () => {
 };
 document.addEventListener("DOMContentLoaded", showCards);
 
-
 // Hackathon Section
-
 
 const hackathonsection = document.querySelector(".hackathon-section");
 const mentor = [
@@ -203,15 +201,13 @@ const mentor = [
     subtitle: "Judge",
     image: "assets/images/experience-page/nhacks.png",
     desp: "NHacks VI was a hackathon that aimed to encourage students everywhere to develop skills in computer science by creating tangible products that they're proud of. It initially started as Michigan's largest high school hackathon.",
-  }
-  
-
+  },
 ];
 
 const showCards3 = () => {
   let output = "";
   mentor.forEach(
-    ({ title, image, subtitle, desp}) =>
+    ({ title, image, subtitle, desp }) =>
       (output += `  
       <div class="blog-slider__item swiper-slide">
         <div class="blog-slider__img">
